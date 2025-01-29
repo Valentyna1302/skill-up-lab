0941e509b2c45ed3f5566690ab3b79f59b316ae7
Valentyna1302 <122233669+Valentyna1302@users.noreply.github.com>
2025-01-29 15:06:43 +0000

    Deploying to gh-pages from @ Valentyna1302/skill-up-lab@8981b31923c478556ac29fbae11a0622cad8aeeb 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/styles-B37gaLlg.js";const r=document.querySelector(".form");r.addEventListener("submit",e=>{e.preventDefault,new Promise((t,o)=>{})});
+//# sourceMappingURL=2-snackbar.js.map
