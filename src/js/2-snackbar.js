8981b31923c478556ac29fbae11a0622cad8aeeb
Valentyna1302 <122233669+Valentyna1302@users.noreply.github.com>
2025-01-29 16:06:08 +0100

    update

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault;
+
+  const promise = new Promise((resolve, reject) => {
+    // Asynchronous operation
+  });
+});
