8ecde76212f818a2ded85d7fa9c0b82799410f51
Valentyna1302 <122233669+Valentyna1302@users.noreply.github.com>
2025-01-30 12:25:19 +0000

    Deploying to gh-pages from @ Valentyna1302/skill-up-lab@1c1c813a5e966211bb974d1df0e7721eb5614c66 🚀

--- 2-snackbar.js
+++ 2-snackbar.js
@@ -1,2 +1,2 @@
-import"./assets/styles-B37gaLlg.js";const r=document.querySelector(".form");r.addEventListener("submit",e=>{e.preventDefault,new Promise((t,o)=>{})});
+import"./assets/styles-B37gaLlg.js";const t={title:"CRUD",body:"CRUD is awesome!"},e={method:"POST",body:JSON.stringify(t),headers:{"Content-Type":"application/json; charset=UTF-8"}};fetch("https://jsonplaceholder.typicode.com/posts",e).then(o=>{if(!o.ok)throw new Error(o.status);return o.json()}).then(o=>console.log(o)).catch(o=>console.log(o));
 //# sourceMappingURL=2-snackbar.js.map
